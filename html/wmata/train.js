f3f262bbf75e746527e1359623e2fc1257a680c9
citymaus <citymaus@users.noreply.github.com>
2018-02-07 23:30:31 -0500

    Bug fixes, adding station info

--- html/wmata/train.js
+++ html/wmata/train.js
@@ -8,18 +8,43 @@
 		LocationCode:"E04"
 		LocationName:"Columbia Heights"
 		Min:"BRD"
-  */
+  */  
+ 
+class StationInfo {
+	constructor(code, openingTime, day) {	
+		this.code = code;
+		this.openingTime = openingTime;
+		this.day = day;
+		this.firstTrains = {};	// "A01": "00:00"
+		this.lastTrains = {};
+	}
+	addFirstTrains(firstTrainList) {
+		for (var i = 0, len = firstTrainList.length; i < len; i++) {		
+			if (this.firstTrains.length == 0 || !(firstTrainList[i].DestinationStation in this.firstTrains)) {
+				this.firstTrains[firstTrainList[i].DestinationStation] = firstTrainList[i].Time;
+			}	
+		}
+	}
+	addLastTrains(lastTrainList) {
+		for (var i = 0, len = lastTrainList.length; i < len; i++) {		
+			if (this.lastTrains.length == 0 || !(lastTrainList[i].DestinationStation in this.lastTrains)) {
+				this.lastTrains[lastTrainList[i].DestinationStation] = lastTrainList[i].Time;
+			}	
+		}
+	}
+}
+ 
 class MetroStation {
   constructor(locationName, locationCode) {
     this.locationName = locationName;
     this.locationCode = locationCode;
 	this.locationCodes = {};		
 	this.trainGroup = {};		// Group:"1", class TrainGroup
+	this.trainProps = new TrainProperties();
 	
-	$.getScript('static_properties.js', function()
-	{
-		this.trainProps = new TrainProperties();
-	});
+	this.weekdayInfo = {};		// "weekday", class StationInfo
+	this.saturdayInfo = {};
+	this.sundayInfo = {};
   }
   
   addLocationCode(locationCode) {
@@ -34,6 +59,11 @@ class MetroStation {
 	  }
 	  var trainTrack = this.trainGroup[direction];	  
 	  trainTrack.addTrain(line, cars, min, destinationCode, destinationName);
+  }
+  addInformation(openingTime, firstTrains, lastTrains) {
+	  this.openingTime = openingTime;
+	  this.firstTrains = firstTrains;
+	  this.lastTrains = lastTrains;
   }
   getTrains(direction) {
 	  var trains = [];
@@ -103,5 +133,70 @@ class MetroTrain {
 		this.Min = min;
 		this.DestinationCode = destinationCode;
 		this.DestinationName = destinationName;
+	}
+}
+
+/* -------------------------------------------------------------------- 
+* STATIC METRO VALUES 
+* ---------------------------------------------------------------------
+*/
+
+class TrainProperties {
+  constructor() {
+  }
+	getDirectionIcon(direction) {
+	  var iconClass = "fas fa-question-circle";	  
+	  if (direction == "south") {
+		iconClass = "fas fa-angle-double-down";
+	  }
+	  if (direction == "north") {
+		iconClass = "fas fa-angle-double-up";
+	  }
+	  if (direction == "east") {
+		iconClass = "fas fa-angle-double-right";		  
+	  }
+	  if (direction == "west") {
+		iconClass = "fas fa-angle-double-left";		  
+	  }
+	  return iconClass;
+	}
+	
+	getLineDirection(destinationCode) {	 
+		var direction = "";
+		var northDestinations = {
+			"E06": "Fort Totten",
+			"E10": "Greenbelt"
+		};
+		var southDestinations = {
+			"F11": "Branch Ave",
+			"C15": "Huntington"
+		};
+		var eastDestinations = {
+			"B11": "Glenmont",
+			"B08": "Silver Spring",
+			"D13": "New Carrollton",
+			"G05": "Largo Town Center"
+		};
+		var westDestinations = {
+			"A15": "Shady Grove",
+			"A11": "Grosvenor-Strathmore",
+			"N06": "Wiehle-Reston East",
+			"K08": "Vienna",
+			"J03": "Franconia-Springfield"
+		};
+
+		if (destinationCode in northDestinations) {
+			direction = "north"; 
+		}
+		if (destinationCode in southDestinations) {
+			direction = "south"; 
+		}
+		if (destinationCode in eastDestinations) {
+			direction = "east"; 
+		}
+		if (destinationCode in westDestinations) {
+			direction = "west"; 
+		}
+		return direction;
 	}
 }
