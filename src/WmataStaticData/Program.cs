d2e904b959a074d8e4e46a55071cd34b53cdf2bd
citymaus <citymaus@users.noreply.github.com>
2019-09-10 23:22:57 -0400

    Adding static weather data builder

--- src/WmataStaticData/Program.cs
+++ src/WmataStaticData/Program.cs
@@ -1,5 +1,5 @@
-﻿#undef STATICWMATA
-#define STATICDARKSKY
+﻿#define STATICWMATA
+#undef STATICDARKSKY
 using System;
 
 namespace WmataStaticData
@@ -8,19 +8,27 @@ namespace WmataStaticData
     {
         static void Main(string[] args)
         {
+            var overwriteData = true;
+            
             var wmataApiKey = new ApiKeyHelper("WMATA").ApiKey;
             var darkSkyApiKey = new ApiKeyHelper("DARKSKY").ApiKey;
 
+            Console.WriteLine("Creating static files...");
+            Console.WriteLine();
+
 #if STATICWMATA
-            var stationDataBuilder = new StationDataBuilder(wmataApiKey);
-            var stopDataBuilder = new StopDataBuilder(wmataApiKey);
-            stationDataBuilder.MakeStationRequest();
-            stopDataBuilder.MakeStopRequest();
+            var stationDataBuilder = new StationDataBuilder(wmataApiKey, overwriteData);
+            var stopDataBuilder = new StopDataBuilder(wmataApiKey, overwriteData);
+            stationDataBuilder.MakeStationRequest().Wait();
+            stopDataBuilder.MakeStopRequest().Wait();
 #endif
+
 #if STATICDARKSKY
-            var staticWeatherDataBuilder = new StaticWeatherDataBuilder(darkSkyApiKey);
+            var staticWeatherDataBuilder = new StaticWeatherDataBuilder(darkSkyApiKey, overwriteData);
             staticWeatherDataBuilder.MakeWeatherRequest();
 #endif
+
+            Console.WriteLine();
             Console.WriteLine("Hit ENTER to exit...");
             Console.ReadLine();
         }
